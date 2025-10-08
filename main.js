7f59f8c89a666729d9ccbae4b9072c356814ac64
Steven Taveras <dreamsniper@users.noreply.github.com>
2025-10-08 20:19:41 +0000

    updated import

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import StartMenuScene from "scenes/StartMenuScene.js";
-import GameScene from "scenes/GameScene.js";
+import StartMenuScene from "./StartMenuScene.js";
+import GameScene from "./GameScene.js";
 
 const config = {
   type: Phaser.AUTO,
