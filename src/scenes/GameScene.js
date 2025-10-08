3e5eb2bb898e7d5c89a673a0893e4848ef034edb
Steven Taveras <dreamsniper@users.noreply.github.com>
2025-10-08 16:39:11 +0000

    reorganized

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,34 +1,41 @@
 export default class GameScene extends Phaser.Scene {
-    constructor() {
-        super("GameScene");
-        this.score = 0;
-    }
+  constructor() {
+    super("GameScene");
+    this.score = 0;
+  }
 
-    preload() {
+  preload() {
     this.load.spritesheet("bird", "assets/images/bird-spritesheet.png", {
-        frameWidth: 34, frameHeight: 24
+      frameWidth: 34,
+      frameHeight: 24,
     });
     this.load.image("pipe", "assets/images/pipe.png");
+    this.load.image("bg-dawn", "assets/images/background-dawn.png"); // Background
+  }
 
-    // Background
-    this.load.image("bg-dawn", "assets/images/background-dawn.png");
-}
-
-    create() {
+  create() {
     // Background (tileSprite lets us scroll it)
-    this.bg = this.add.tileSprite(0, 0, this.sys.game.config.width, this.sys.game.config.height, "bg-dawn")
-        .setOrigin(0, 0);
+    this.bg = this.add
+      .tileSprite(
+        0,
+        0,
+        this.sys.game.config.width,
+        this.sys.game.config.height,
+        "bg-dawn"
+      )
+      .setOrigin(0, 0);
 
     // Bird
     this.bird = this.physics.add.sprite(100, 300, "bird");
     this.bird.setCollideWorldBounds(true);
 
     this.anims.create({
-        key: "flap",
-        frames: this.anims.generateFrameNumbers("bird", { start: 0, end: 2 }),
-        frameRate: 10,
-        repeat: -1
+      key: "flap",
+      frames: this.anims.generateFrameNumbers("bird", { start: 0, end: 2 }),
+      frameRate: 10,
+      repeat: -1,
     });
+
     this.bird.play("flap");
 
     // Input
@@ -38,76 +45,66 @@ export default class GameScene extends Phaser.Scene {
     // Pipes
     this.pipes = this.physics.add.group();
     this.time.addEvent({
-        delay: 1500,
-        callback: this.spawnPipe,
-        callbackScope: this,
-        loop: true
+      delay: 1500,
+      callback: this.spawnPipe,
+      callbackScope: this,
+      loop: true,
     });
 
     // Score
     this.score = 0;
     this.scoreText = this.add.text(20, 20, "Score: 0", {
-        fontSize: "24px",
-        fill: "#fff"
+      fontSize: "24px",
+      fill: "#fff",
     });
 
     // Collision
     this.physics.add.overlap(this.bird, this.pipes, this.gameOver, null, this);
-}
+  }
 
-    flap() {
-        this.bird.setVelocityY(-300);
+  flap() {
+    this.bird.setVelocityY(-300);
+  }
+
+  spawnPipe() {
+    const gap = 140;
+    const y = Phaser.Math.Between(150, 450);
+
+    // Top pipe
+    const topPipe = this.pipes.create(400, y - gap, "pipe").setOrigin(0, 1);
+    topPipe.flipY = true;
+    topPipe.body.allowGravity = false;
+    topPipe.body.setVelocityX(-200);
+
+    // Bottom pipe
+    const bottomPipe = this.pipes.create(400, y + gap, "pipe").setOrigin(0, 0);
+    bottomPipe.body.allowGravity = false;
+    bottomPipe.body.setVelocityX(-200);
+
+    // Score zone
+    const scoreZone = this.add.zone(400, y, 1, this.sys.game.config.height);
+    this.physics.world.enable(scoreZone);
+    scoreZone.body.allowGravity = false;
+    scoreZone.body.setVelocityX(-200);
+
+    this.physics.add.overlap(this.bird, scoreZone, () => {
+      this.score++;
+      this.scoreText.setText("Score: " + this.score);
+      scoreZone.destroy();
+    });
+  }
+
+  gameOver() {
+    this.scene.restart();
+  }
+
+  update() {
+    // Scroll background
+    this.bg.tilePositionX += 1;
+
+    // If bird hits top/bottom
+    if (this.bird.y >= 600 || this.bird.y <= 0) {
+      this.gameOver();
     }
-
-    spawnPipe() {
-        const gap = 140;
-        const minY = gap + 50;
-        const maxY = this.sys.game.config.height - gap - 50;
-        const y = Phaser.Math.Between(minY, maxY);
-
-
-        // Top pipe
-        let topPipe = this.pipes.create(400, y - gap, "pipe").setOrigin(0, 1);
-        // Flip the sprite upside down
-        topPipe.flipY = true;
-        topPipe.body.allowGravity = false;
-        topPipe.body.setVelocityX(-200);
-
-        // Bottom pipe
-        let bottomPipe = this.pipes.create(400, y + gap, "pipe").setOrigin(0, 0);
-        bottomPipe.body.allowGravity = false;
-        bottomPipe.body.setVelocityX(-200);
-
-        // Score zone
-        let scoreZone = this.add.zone(400, y, 1, gap * 2);
-        this.physics.world.enable(scoreZone);
-        scoreZone.body.allowGravity = false;
-        scoreZone.body.setVelocityX(-200);
-
-        this.physics.add.overlap(this.bird, scoreZone, () => {
-            this.score++;
-            this.scoreText.setText("Score: " + this.score);
-            scoreZone.destroy();
-        });
-    }
-    
-    gameOver() {
-        this.scene.restart();
-    }
-
-    update() {
-        // Scroll background
-        this.bg.tilePositionX += 1;
-
-        // If bird hits top/bottom
-        if (this.bird.y >= 600 || this.bird.y <= 0) {
-            this.gameOver();
-        }
-
-        this.pipes.children.each(pipe => {
-            if (pipe.x < -50) {
-                pipe.destroy();
-            }
-        }, this);
-    }
+  }
 }
